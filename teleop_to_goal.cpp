25b5625e42be1eafbbd16d72beb9dc73d100b2d5
bsa0322 <bsa0322@ewhain.net>
2021-08-02 20:19:07 +0900

    실제적용수정

--- teleop_to_goal.cpp
+++ teleop_to_goal.cpp
@@ -3,6 +3,8 @@
 #include <nav_msgs/Odometry.h>
 #include <std_msgs/String.h>
 
+#include <cmath>
+
 // Reminder message
 const char* msg = R"(
 
@@ -10,6 +12,7 @@ This code move turtlebot straight.
 
 )";
 
+//amcl
 // Init variables
 float target_linear_vel   = 0.1;
 double current_x = 0; 
@@ -17,7 +20,7 @@ double goal_x = 0.5;
 
 void odomCallback(const nav_msgs::Odometry::ConstPtr& odom)
 {
-  ROS_INFO("I received odom: [%f, %f, %f]", odom->pose.pose.position.x, odom->pose.pose.position.y, odom->pose.pose.position.z); //받았다는 걸 명시...!
+  ROS_INFO("I received odom: [%f, %f, %f] goal : %f", current_x, odom->pose.pose.position.y, odom->pose.pose.position.z, goal_x); //받았다는 걸 명시...!
   current_x = odom->pose.pose.position.x;
 }
 
@@ -28,16 +31,17 @@ int main(int argc, char** argv)
   ros::NodeHandle nh;
 
   // Init cmd_vel publisher
-  ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+  ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
   // Create Twist message
   geometry_msgs::Twist twist;
 
   // Init odom subsciber
-  ros::Subscriber sub = nh.subscribe("odom", 1000, odomCallback);  
+  ros::Subscriber sub = nh.subscribe("/odom", 1000, odomCallback);  
 
   printf("%s", msg);
 
-  while(current_x < goal_x){
+  while(std::abs(goal_x - current_x) > 0.1){
+    target_linear_vel = (goal_x - current_x) * 0.1;
 
     // Update the Twist message
     twist.linear.x = target_linear_vel;
@@ -50,12 +54,22 @@ int main(int argc, char** argv)
 
     // Publish it and resolve any remaining callbacks
     pub.publish(twist);
+
     ros::spinOnce();
     
   }
-
+  
+  printf("here\n");
   twist.linear.x = 0;
+  twist.linear.y = 0;
+  twist.linear.z = 0;
+  twist.angular.x = 0;
+  twist.angular.y = 0;
+  twist.angular.z = 0;
+
   pub.publish(twist);
+  ros::spinOnce();
+  
 
   return 0;
 }
