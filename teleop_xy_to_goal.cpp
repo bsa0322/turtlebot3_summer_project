64b01f7727c23069d30441ae1823f1992089a50d
bsa0322 <bsa0322@ewhain.net>
2021-08-03 16:30:11 +0900

    xy_goal코드조금정리

--- teleop_xy_to_goal.cpp
+++ teleop_xy_to_goal.cpp
@@ -3,6 +3,7 @@
 #include <nav_msgs/Odometry.h>
 #include <std_msgs/String.h>
 
+#include <stdio.h>
 #include <cmath>
 
 // Reminder message
@@ -12,45 +13,22 @@ This code move turtlebot straight.
 
 )";
 
-// Init variables
-#define LINEAR_VEL_SIZE 0.05
-#define ANGULAR_VEL_SIZE 0.07
+ros::Publisher pub;
+
+// Create Twist message
+geometry_msgs::Twist twist;
 
 double current_x = 0; 
 double current_y = 0;
 double current_angular_z = 0;
-double goal_x = 0.4; //골 x좌표
-double goal_y = 0.3; //골 y좌표
 double goal_ang; //골 angular
 
-void odomCallback(const nav_msgs::Odometry::ConstPtr& odom)
+void to_goal(double goal_x, double goal_y)
 {
-  ROS_INFO("I received odom: [%f, %f, %f]", odom->pose.pose.position.x, odom->pose.pose.position.y, odom->pose.pose.orientation.z); //받았다는 걸 명시...!
-  current_x = odom->pose.pose.position.x;
-  current_y = odom->pose.pose.position.y;
-  current_angular_z = odom->pose.pose.orientation.z;
-
-}
-
-int main(int argc, char** argv)
-{
-  // Init ROS node
-  ros::init(argc, argv, "turtlebot3_to_goal");
-  ros::NodeHandle nh;
-
-  // Init cmd_vel publisher
-  ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
-  // Create Twist message
-  geometry_msgs::Twist twist;
-
-  // Init odom subsciber
-  ros::Subscriber sub = nh.subscribe("odom", 1000, odomCallback);  
-
-  printf("%s", msg);
-
   goal_ang = std::atan(goal_y/goal_x) / 2;
 
-  while(abs(goal_x-current_x) > 0.02 || abs(goal_y-current_y) > 0.02){
+  while(abs(goal_x-current_x) > 0.02 || abs(goal_y-current_y) > 0.02)
+  {
 
     if(current_angular_z <= goal_ang)
     {
@@ -73,6 +51,39 @@ int main(int argc, char** argv)
   twist.linear.x = 0;
   twist.angular.z = 0;
   pub.publish(twist);
+}
+
+void odomCallback(const nav_msgs::Odometry::ConstPtr& odom)
+{
+  ROS_INFO("I received odom: [%f, %f, %f]", odom->pose.pose.position.x, odom->pose.pose.position.y, odom->pose.pose.orientation.z); //받았다는 걸 명시...!
+  current_x = odom->pose.pose.position.x;
+  current_y = odom->pose.pose.position.y;
+  current_angular_z = odom->pose.pose.orientation.z;
+
+}
+
+int main(int argc, char** argv)
+{
+  // Init ROS node
+  ros::init(argc, argv, "turtlebot3_xy_goal");
+  ros::NodeHandle nh;
+
+  // Init cmd_vel publisher
+  pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 10);
+
+  // Init odom subsciber
+  ros::Subscriber sub = nh.subscribe("odom", 1000, odomCallback);  
+
+  printf("%s", msg);
+
+  double goal_x, goal_y;
+  std::cout<<"목표 x 좌표: ";
+  std::cin>>goal_x;
+  std::cout<<"목표 y 좌표: ";
+  std::cin>>goal_y;
+
+  to_goal(goal_x,goal_y);
 
   return 0;
 }
+
