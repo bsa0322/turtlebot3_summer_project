31e83fd778460a6193cb835bd11a7351f2c41a94
bsa0322 <bsa0322@ewhain.net>
2021-08-03 14:51:24 +0900

    실제로봇적용(시간사용)ok

--- teleop_usetime_linear.cpp
+++ teleop_usetime_linear.cpp
@@ -5,12 +5,11 @@
 #include <stdio.h>
 #include <cmath>
 
-ros::Publisher pub;
+ros::Publisher pub; 
+geometry_msgs::Twist twist;
 
 void move(double speed, double distance, int isForward)
 {
-  geometry_msgs::Twist twist;
-
   if(isForward)
   {
     twist.linear.x = speed;
@@ -30,20 +29,30 @@ void move(double speed, double distance, int isForward)
   while(time == 0) time = ros::Time::now().toSec();
   printf("\n%f\n", time);
   double current_distance = 0;
-  //ros::Rate loop_rate(10);
+  ros::Rate loop_rate(10); //이거 없으면 실제 로봇에서 적용 안됨, 안 멈춤.
 
   while(current_distance <= distance)
   {
-    pub.publish(twist);
     double time2 = ros::Time::now().toSec();
     printf("%f\n", time2);
     current_distance = (time2-time) * speed;
-    ros::spinOnce();
-    //loop_rate.sleep();
+    pub.publish(twist);
+    ros::spinOnce(); //없어도 잘 실행되긴 함.
+    loop_rate.sleep(); //이거 없으면 실제 로봇에서 적용 안됨, 안 멈춤.
   }
 
+  //twist = geometry_msgs::Twist(); //twist 초기화,,?!
   twist.linear.x = 0;
+  twist.linear.y = 0;
+  twist.linear.z = 0;
+  twist.angular.x = 0;
+  twist.angular.y = 0;
+  twist.angular.z = 0;
+
   pub.publish(twist);
+  //ros::spin();
+
+  printf("x선 속도: %f\n",twist.linear.x);
   
 }
 
@@ -54,7 +63,7 @@ int main(int argc, char** argv)
   ros::NodeHandle nh;
 
   // Init cmd_vel publisher
-  pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+  pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 10); //topic에 /있으나 없으나 상관 없다.
 
   double speed, distance;
   int isForward;
@@ -67,7 +76,8 @@ int main(int argc, char** argv)
   std::cin>>isForward;
   
   move(speed, distance, isForward);
+
+  //ros::spin();
   
   return 0;
 }
-
