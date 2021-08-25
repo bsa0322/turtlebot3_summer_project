4106a6f9999ef6568462e2624e4f4b892ee90217
bsa0322 <bsa0322@ewhain.net>
2021-08-25 18:49:40 +0900

    raspicam_opencv영상연결안됌

--- yolov3_deepsort.py
+++ yolov3_deepsort.py
@@ -21,11 +21,13 @@ from utils.parser import get_config
 from utils.log import get_logger
 from utils.io import write_results
 
+_video_path=None
+
 class VideoTracker(object):
-    def __init__(self, cfg, args, video_path):
+    def __init__(self, cfg, args):
         self.cfg = cfg
         self.args = args
-        self.video_path = video_path
+        
         self.logger = get_logger("root")
 
         use_cuda = args.use_cuda and torch.cuda.is_available()
@@ -36,10 +38,8 @@ class VideoTracker(object):
             cv2.namedWindow("test", cv2.WINDOW_NORMAL)
             cv2.resizeWindow("test", args.display_width, args.display_height)
             
-
         if args.cam != -1:
             print("Using webcam " + str(args.cam))
-            self.vdo = cv2.imdecode(video_path, cv2.IMREAD_COLOR) 
         
         self.detector = build_detector(cfg, use_cuda=use_cuda)
         self.deepsort = build_tracker(cfg, use_cuda=use_cuda)
@@ -53,73 +53,77 @@ class VideoTracker(object):
             print(exc_type, exc_value, exc_traceback)
 
     def run(self):
-        results = []
-        idx_frame = 0
-        #while True:
-        idx_frame += 1
-        #if idx_frame % self.args.frame_interval:
-        #    continue
+        while True:
+            self.video_path = _video_path
+            results = []
+            idx_frame = 0
+            #while True:
+            idx_frame += 1
+            #if idx_frame % self.args.frame_interval:
+            #    continue
 
-        start = time.time()
+            start = time.time()
 
+                
+            #_, ori_im = self.vdo.retrieve()
+
+            #ros->opencv
+                
+            print("video path: ",self.video_path)
+            ori_im = cv2.imdecode(self.video_path, cv2.IMREAD_COLOR) 
+            if ori_im.data == False:
+                print("Image not found")
+                break
+                
+            #cv2.imshow('cv_img',ori_im)
+            #cv2.waitKey(2)
+            im = cv2.cvtColor(ori_im, cv2.COLOR_BGR2RGB)
+                
+
+            #카메라 크기 얻어옴
+            width = np.size(ori_im, 1)
+            height = np.size(ori_im, 0)
+            print("카메라 크기",width,height)
+                
+
+            # do detection
+            bbox_xywh, cls_conf, cls_ids = self.detector(im)
+
+            # select person class
+            mask = cls_ids == 0
+
+            bbox_xywh = bbox_xywh[mask]
+            # bbox dilation just in case bbox too small, delete this line if using a better pedestrian detector
+            bbox_xywh[:, 3:] *= 1.2
+            cls_conf = cls_conf[mask]
+
+            # do tracking
+            outputs = self.deepsort.update(bbox_xywh, cls_conf, im)
+
+            # draw boxes for visualization
+            if len(outputs) > 0:
+                bbox_tlwh = []
+                bbox_xyxy = outputs[:, :4]
+                identities = outputs[:, -1]
+                ori_im = draw_boxes(width, height, ori_im, bbox_xyxy, identities)
+
+                for bb_xyxy in bbox_xyxy:
+                    bbox_tlwh.append(self.deepsort._xyxy_to_tlwh(bb_xyxy))
+
+                results.append((idx_frame - 1, bbox_tlwh, identities))
+
+            end = time.time()
+
+            if self.args.display:
+                cv2.imshow("test", ori_im)
+                cv2.waitKey(1)
+
+            # logging
+            self.logger.info("time: {:.03f}s, fps: {:.03f}, detection numbers: {}, tracking numbers: {}" \
+                                .format(end - start, 1 / (end - start), bbox_xywh.shape[0], len(outputs)))
             
-        #_, ori_im = self.vdo.retrieve()
-
-        #ros->opencv
-            
-        print("video path: ",self.video_path)
-        ori_im = cv2.imdecode(self.video_path, cv2.IMREAD_COLOR) 
-        if ori_im.data == False:
-            print("Image not found")
-            #break
-            
-        cv2.imshow('cv_img',ori_im)
-        cv2.waitKey(2)
-        im = cv2.cvtColor(ori_im, cv2.COLOR_BGR2RGB)
-            
-
-        #카메라 크기 얻어옴
-        width = np.size(ori_im, 1)
-        height = np.size(ori_im, 0)
-        print("카메라 크기",width,height)
-            
-
-        # do detection
-        bbox_xywh, cls_conf, cls_ids = self.detector(im)
-
-        # select person class
-        mask = cls_ids == 0
-
-        bbox_xywh = bbox_xywh[mask]
-        # bbox dilation just in case bbox too small, delete this line if using a better pedestrian detector
-        bbox_xywh[:, 3:] *= 1.2
-        cls_conf = cls_conf[mask]
-
-        # do tracking
-        outputs = self.deepsort.update(bbox_xywh, cls_conf, im)
-
-        # draw boxes for visualization
-        if len(outputs) > 0:
-            bbox_tlwh = []
-            bbox_xyxy = outputs[:, :4]
-            identities = outputs[:, -1]
-            ori_im = draw_boxes(width, height, ori_im, bbox_xyxy, identities)
-
-            for bb_xyxy in bbox_xyxy:
-                bbox_tlwh.append(self.deepsort._xyxy_to_tlwh(bb_xyxy))
-
-            results.append((idx_frame - 1, bbox_tlwh, identities))
-
-        end = time.time()
-
-        if self.args.display:
-            cv2.imshow("test", ori_im)
-            cv2.waitKey(1)
-
-        # logging
-        self.logger.info("time: {:.03f}s, fps: {:.03f}, detection numbers: {}, tracking numbers: {}" \
-                            .format(end - start, 1 / (end - start), bbox_xywh.shape[0], len(outputs)))
-        rospy.spin()
+            #rospy.spin()
+        
 
 
 def parse_args():
@@ -140,33 +144,25 @@ def parse_args():
 
 #ros -> opencv
 class image_feature:
-      def __init__(self):
+      def __init__(self, vt):
             rospy.init_node('image_feature', anonymous=True)
             self.subscriber = rospy.Subscriber("/raspicam_node/image/compressed", CompressedImage, self.callback, queue_size = 1)
-           
+            #self.vt = vt
+      
       def callback(self, ros_data):
-
+        global _video_path
         _video_path = np.fromstring(ros_data.data, np.uint8)
-        
-        try:
-            args = parse_args()
-            cfg = get_config()
-            cfg.merge_from_file(args.config_detection)
-            cfg.merge_from_file(args.config_deepsort)
-            
-            print("ros work")
-            with VideoTracker(cfg, args, _video_path) as vdo_trk:
-                vdo_trk.run()
-            
-        except KeyboardInterrupt:
-            print ("Shutting down ROS Image feature detector module")
 
 if __name__ == "__main__":
     #ros->opencv
-    ic = image_feature()
-    #vt = VideoTracker()
+    args = parse_args()
+    cfg = get_config()
+    cfg.merge_from_file(args.config_detection)
+    cfg.merge_from_file(args.config_deepsort)
+    vt = VideoTracker(cfg, args)
+    ic = image_feature(vt)
     rospy.spin()
-    
+    vt.run()
 
     
 
