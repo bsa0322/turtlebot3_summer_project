f9d79aab4ca1078a2fccaf4f6e7d51c477170533
bsa0322 <bsa0322@ewhain.net>
2021-08-25 21:01:34 +0900

    raspicam_opencv영상연결성공!

--- yolov3_deepsort.py
+++ yolov3_deepsort.py
@@ -22,6 +22,7 @@ from utils.log import get_logger
 from utils.io import write_results
 
 _video_path=None
+chk=0
 
 class VideoTracker(object):
     def __init__(self, cfg, args):
@@ -54,76 +55,75 @@ class VideoTracker(object):
 
     def run(self):
         while True:
-            self.video_path = _video_path
             results = []
             idx_frame = 0
+            
             #while True:
-            idx_frame += 1
+            #idx_frame += 1
             #if idx_frame % self.args.frame_interval:
             #    continue
 
             start = time.time()
 
-                
             #_, ori_im = self.vdo.retrieve()
 
             #ros->opencv
                 
-            print("video path: ",self.video_path)
-            ori_im = cv2.imdecode(self.video_path, cv2.IMREAD_COLOR) 
-            if ori_im.data == False:
-                print("Image not found")
-                break
+            ori_im = None;
+            if chk == 0:
+                continue
+            ori_im = cv2.imdecode(_video_path, cv2.IMREAD_COLOR) 
+            print("video path: ",_video_path)
+            if not ori_im.data == False:
                 
-            #cv2.imshow('cv_img',ori_im)
-            #cv2.waitKey(2)
-            im = cv2.cvtColor(ori_im, cv2.COLOR_BGR2RGB)
+                print("Image found")
+                #break
                 
+                #cv2.imshow('cv_img',ori_im)
+                #cv2.waitKey(2) 
+                im = cv2.cvtColor(ori_im, cv2.COLOR_BGR2RGB)
+                    
 
-            #카메라 크기 얻어옴
-            width = np.size(ori_im, 1)
-            height = np.size(ori_im, 0)
-            print("카메라 크기",width,height)
-                
+                #카메라 크기 얻어옴
+                width = np.size(ori_im, 1)
+                height = np.size(ori_im, 0)
+                print("카메라 크기",width,height)
+                        
+                # do detection
+                bbox_xywh, cls_conf, cls_ids = self.detector(im)
 
-            # do detection
-            bbox_xywh, cls_conf, cls_ids = self.detector(im)
+                # select person class
+                mask = cls_ids == 0
 
-            # select person class
-            mask = cls_ids == 0
+                bbox_xywh = bbox_xywh[mask]
+                # bbox dilation just in case bbox too small, delete this line if using a better pedestrian detector
+                bbox_xywh[:, 3:] *= 1.2                   
+                cls_conf = cls_conf[mask]
 
-            bbox_xywh = bbox_xywh[mask]
-            # bbox dilation just in case bbox too small, delete this line if using a better pedestrian detector
-            bbox_xywh[:, 3:] *= 1.2
-            cls_conf = cls_conf[mask]
+                # do tracking
+                outputs = self.deepsort.update(bbox_xywh, cls_conf, im)
 
-            # do tracking
-            outputs = self.deepsort.update(bbox_xywh, cls_conf, im)
+                print("outputs: ", outputs)
+                # draw boxes for visualization
+                if len(outputs) > 0:
+                    bbox_tlwh = []
+                    bbox_xyxy = outputs[:, :4]
+                    identities = outputs[:, -1]
+                    ori_im = draw_boxes(width, height, ori_im, bbox_xyxy, identities)
 
-            # draw boxes for visualization
-            if len(outputs) > 0:
-                bbox_tlwh = []
-                bbox_xyxy = outputs[:, :4]
-                identities = outputs[:, -1]
-                ori_im = draw_boxes(width, height, ori_im, bbox_xyxy, identities)
+                    for bb_xyxy in bbox_xyxy:
+                        bbox_tlwh.append(self.deepsort._xyxy_to_tlwh(bb_xyxy))
 
-                for bb_xyxy in bbox_xyxy:
-                    bbox_tlwh.append(self.deepsort._xyxy_to_tlwh(bb_xyxy))
+                    results.append((idx_frame - 1, bbox_tlwh, identities))
 
-                results.append((idx_frame - 1, bbox_tlwh, identities))
+                end = time.time()
 
-            end = time.time()
-
-            if self.args.display:
                 cv2.imshow("test", ori_im)
-                cv2.waitKey(1)
+                cv2.waitKey(2)
 
-            # logging
-            self.logger.info("time: {:.03f}s, fps: {:.03f}, detection numbers: {}, tracking numbers: {}" \
-                                .format(end - start, 1 / (end - start), bbox_xywh.shape[0], len(outputs)))
-            
-            #rospy.spin()
-        
+                # logging
+                self.logger.info("time: {:.03f}s, fps: {:.03f}, detection numbers: {}, tracking numbers: {}" \
+                                    .format(end - start, 1 / (end - start), bbox_xywh.shape[0], len(outputs)))
 
 
 def parse_args():
@@ -143,15 +143,14 @@ def parse_args():
 
 
 #ros -> opencv
-class image_feature:
-      def __init__(self, vt):
-            rospy.init_node('image_feature', anonymous=True)
-            self.subscriber = rospy.Subscriber("/raspicam_node/image/compressed", CompressedImage, self.callback, queue_size = 1)
-            #self.vt = vt
-      
-      def callback(self, ros_data):
-        global _video_path
-        _video_path = np.fromstring(ros_data.data, np.uint8)
+def callback(ros_data):
+    global chk
+    chk=1
+    #rate = rospy.Rate(10)
+    global _video_path
+    _video_path = np.fromstring(ros_data.data, np.uint8)
+    print(_video_path)
+    #rate.sleep()
 
 if __name__ == "__main__":
     #ros->opencv
@@ -160,9 +159,18 @@ if __name__ == "__main__":
     cfg.merge_from_file(args.config_detection)
     cfg.merge_from_file(args.config_deepsort)
     vt = VideoTracker(cfg, args)
-    ic = image_feature(vt)
-    rospy.spin()
+
+    rospy.init_node('image_feature', anonymous=True)
+    subscriber = rospy.Subscriber("/raspicam_node/image/compressed", CompressedImage, callback, queue_size = 1)
+    
     vt.run()
+    
+    rate = rospy.Rate(1)
+    rospy.spin()
+    rate.sleep()
 
     
+
+    
+
 
